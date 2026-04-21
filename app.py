b0fca4bc7de95aa557032c78cefbb6d789b7a289
Bryce <bryce.gilmartin@gmail.com>
2026-04-21 16:50:55 -0700

    Clean up imports in app.py
    Removed unused imports related to Flask-Login and Werkzeug.

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import (
-    LoginManager,
-    UserMixin,
-    login_user,
-    login_required,
-    logout_user,
-    current_user,
-)
-from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import os
 
